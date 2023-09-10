ab720787a3eb6fa17c11314b4bcd66702b990c31
Joshua Lam <joshualam071522@gmail.com>
2023-09-09 22:31:28 -0700

    added empty divs for questions and answers, added initial code for js

---
+++ assets/js/index.js
@@ -0,0 +1,58 @@
+const timeEl= document.getElementById('time');
+const startBtn = document.getElementById('start');
+const questionContainerEl = document.getElementById('question-container');
+const answerContainerEl = document.getElementById('answer-container');
+
+
+let score = 0;
+let timeLeft = 60;
+
+let questionIndex = 0;
+
+const questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        answers: [
+            { text: "strings", correct: false },
+            { text: "booleans", correct: false },
+            { text: "alerts", correct: true },
+            { text: "numbers", correct: false }
+        ]
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        answers: [
+            { text: "quotes", correct: false },
+            { text: "curly brackets", correct: false },
+            { text: "parentheses", correct: true },
+            { text: "square brackets", correct: false }
+        ]
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        answers: [
+            { text: "numbers and strings", correct: false },
+            { text: "other arrays", correct: false },
+            { text: "booleans", correct: false },
+            { text: "all of the above", correct: true }
+        ]
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: [
+            { text: "commas", correct: false },
+            { text: "curly brackets", correct: false },
+            { text: "quotes", correct: true },
+            { text: "parentheses", correct: false }
+        ]
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: [
+            { text: "JavaScript", correct: false },
+            { text: "terminal / bash", correct: false },
+            { text: "for loops", correct: false },
+            { text: "console.log", correct: true }
+        ]
+    }
+];
